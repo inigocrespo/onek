1aeb2fac6023196020f30aa6251effc7d9b7b026
inigocresposoria <inigo.cresposoria@thoughtworks.com>
2020-11-14 19:26:47 +0100

    Reads form standard in

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+const q = 113
+
 func main() {
-	fmt.Println("Hello World")
+	var b []byte = make([]byte, 1)
+	for {
+		os.Stdin.Read(b)
+		switch b[0] {
+		case q:
+			return
+		default:
+
+			fmt.Println("I got the byte", b, "("+string(b)+")")
+		}
+	}
 }
