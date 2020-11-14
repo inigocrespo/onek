a719e2a2fcc2d7b1dd6b0bdbf325010c2193c529
inigocresposoria <inigo.cresposoria@thoughtworks.com>
2020-11-14 20:35:22 +0100

    Enters raw mode

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const q = 113
 
 func main() {
+	state, err := terminal.MakeRaw((int)(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer terminal.Restore((int)(os.Stdin.Fd()), state)
+
 	var b []byte = make([]byte, 1)
 	for {
 		os.Stdin.Read(b)
