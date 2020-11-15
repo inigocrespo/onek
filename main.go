25d1ce37e2178bd1cf9c5f4f4a7f7de7e65ce658
inigocresposoria <inigo.cresposoria@thoughtworks.com>
2020-11-15 21:26:37 +0100

    Adds cursor movement

--- main.go
+++ main.go
@@ -7,33 +7,44 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-const q = 113
+var fd = (int)(os.Stdin.Fd())
+
+const (
+	q = 113
+	h = 104
+	j = 106
+	k = 107
+	l = 108
+)
 
 var clearScreen = []byte("\x1b[2J")
 
 type editor struct {
-	rows int
-	cols int
+	rows, cols int
+	cx, cy     int
 }
 
 var e = editor{}
 
 func main() {
-	state, err := terminal.MakeRaw((int)(os.Stdin.Fd()))
+	state, err := terminal.MakeRaw(fd)
 	if err != nil {
 		panic(err)
 	}
-	defer terminal.Restore((int)(os.Stdin.Fd()), state)
+	defer terminal.Restore(fd, state)
 
-	width, height, err := terminal.GetSize((int)(os.Stdin.Fd()))
+	width, height, err := terminal.GetSize(fd)
 	if err != nil {
 		panic(err)
 	}
 
 	e.cols = width
 	e.rows = height
+	e.cx = 0
+	e.cy = 0
 
 	write(clearScreen)
+	refresh()
 
 	fmt.Println(e.cols, " ", e.rows)
 
@@ -43,11 +54,39 @@ func main() {
 		switch b[0] {
 		case q:
 			return
+		case h:
+			if e.cx > 0 {
+				e.cx = e.cx - 1
+				refresh()
+			}
+			break
+		case j:
+			if e.cy < e.rows-1 {
+				e.cy = e.cy + 1
+				refresh()
+			}
+			break
+		case k:
+			if e.cy > 0 {
+				e.cy = e.cy - 1
+				refresh()
+			}
+			break
+		case l:
+			if e.cx < e.cols-1 {
+				e.cx = e.cx + 1
+				refresh()
+			}
+			break
 		default:
-
 			fmt.Println("I got the byte", b, "("+string(b)+")")
 		}
 	}
+}
+
+func refresh() {
+	write(clearScreen)
+	fmt.Print("\x1b[", e.cy+1, ";", e.cx+1, "H")
 }
 
 func write(b []byte) {
